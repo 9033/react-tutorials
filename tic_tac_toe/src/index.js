dc1c3953c89f551f52dfc85e9c70e9dd316dcfc9
9033 <28668509+9033@users.noreply.github.com>
2019-12-15 13:34:50 +0900

    게임 완성하기 index.js

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 /*
+https://ko.reactjs.org/tutorial/tutorial.html
+
 3가지 컴포넌트.
 
 react 컴포넌트는
@@ -10,35 +12,69 @@ prop라는 매개변수를 받고
 render함수를 통해 반환.
 */
 var s="미사일라면방독면⏬"
-  class Square extends React.Component {//<button>을 렌더링. 클릭하면 경고창.
-    constructor(props){//무언가를 ㄱ하기 위해 컴포넌트는 state를 사용.
-      super (props);//생성자에서 super는 반드시.
-      this.state = {
-        value:null,
-      };
-    }
-
-    //setState가 호출되면 React에게 Square가 다시 렌더링하라고 알림.
-    render() {
-      return (
-        <button
-          className="square" 
-          onClick={() => this.setState({value: this.props.value }) }
-        >
-          {this.state.value}
-        </button>
-      );//prop을 전달 : 부모 -> 자식 컴포넌트. this.state.value 의 값을 렌더링.
-    }
+  function Square(props){//Square를 함수 컴포넌트로 변경.
+    return(
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
+  // class Square extends React.Component {//<button>을 렌더링. 클릭하면 경고창.
+  //   //게임의 상태를 유지할 필요가 없음 -> constructor를 지움.
+
+  //   //setState가 호출되면 React에게 Square가 다시 렌더링하라고 알림.
+  //   render() {
+  //     return (
+  //       <button
+  //         className="square" 
+  //         onClick={() => this.props.onClick({value: this.props.value }) }
+  //       >
+  //         {this.props.value}
+  //       </button>
+  //     );//prop을 전달 : 부모 -> 자식 컴포넌트. this.state.value 의 값을 렌더링. 넘겨받은 함수를 실행.
+  //   }
+  // }
   
   class Board extends React.Component {//4각형 9개를 렌더링.
+    constructor(props){
+      super(props);
+      this.state ={
+        squares: Array(...s),
+        xIsNext: true,//다음이 누구인가?
+      };//Board컴포넌트에 게임의 상태를 저장.(O,X,null) 부모 컴포넌트에 공유 state를 정의.
+    }
+
+    handleClick(i){//square를 클릭하면 사각형을 채우게함.
+      const squares = this.state.squares.slice();//불변성을 위해 slice로 사본을 만듬.
+      if (calculateWinner(squares) || (squares[i] in ['❌' , '⭕'])){//승자가 결정되거나, 채워진 칸일때.
+        return;
+      }
+      squares[i] = this.state.xIsNext ? '❌' : '⭕';
+      this.setState({
+        squares: squares,
+        xIsNext: ! this.state.xIsNext//X O가 번갈아서 나타게함.
+      });
+    }
+
     renderSquare(i) {
-      return <Square value={s[i]}/>;//Square에 value prop전달.
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={()=>this.handleClick(i)}
+        />
+      );//Square에 value prop전달. squares의 값을 전달. 클릭하면 Board에서 Square로 함수를 전달.
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      //승자가 결정되면 문구를 표시.
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner){
+        status = 'Winner: ' + winner;
+      } else{
+        status = 'Next player: ' + (this.state.xIsNext ? '❌' : '⭕');
+      }
+        
       return (
         <div>
           <div className="status">{status}</div>
@@ -84,4 +120,23 @@ var s="미사일라면방독면⏬"
     <Game />,
     document.getElementById('root')
   );
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
