79efae9f965d04dbab9cb9d0b1b4931ff474c9af
9033 <28668509+9033@users.noreply.github.com>
2019-12-15 14:09:05 +0900

    react-tutorials : 시간 여행 추가하기 index.js

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -36,48 +36,51 @@ var s="미사일라면방독면⏬"
   // }
   
   class Board extends React.Component {//4각형 9개를 렌더링.
-    constructor(props){
-      super(props);
-      this.state ={
-        squares: Array(...s),
-        xIsNext: true,//다음이 누구인가?
-      };//Board컴포넌트에 게임의 상태를 저장.(O,X,null) 부모 컴포넌트에 공유 state를 정의.
-    }
+    // history state를 Game컴포넌트에 두어서 필요 없음.
+    // constructor(props){
+    //   super(props);
+    //   this.state ={
+    //     squares: Array(...s),
+    //     xIsNext: true,//다음이 누구인가?
+    //   };//Board컴포넌트에 게임의 상태를 저장.(O,X,null) 부모 컴포넌트에 공유 state를 정의.
+    // }
 
-    handleClick(i){//square를 클릭하면 사각형을 채우게함.
-      const squares = this.state.squares.slice();//불변성을 위해 slice로 사본을 만듬.
-      if (calculateWinner(squares) || (squares[i] in ['❌' , '⭕'])){//승자가 결정되거나, 채워진 칸일때.
-        return;
-      }
-      squares[i] = this.state.xIsNext ? '❌' : '⭕';
-      this.setState({
-        squares: squares,
-        xIsNext: ! this.state.xIsNext//X O가 번갈아서 나타게함.
-      });
-    }
+    // Game컴포넌트로 이동.
+    // handleClick(i){//square를 클릭하면 사각형을 채우게함.
+    //   const squares = this.state.squares.slice();//불변성을 위해 slice로 사본을 만듬.
+    //   if (calculateWinner(squares) || (squares[i] in ['❌' , '⭕'])){//승자가 결정되거나, 채워진 칸일때.
+    //     return;
+    //   }
+    //   squares[i] = this.state.xIsNext ? '❌' : '⭕';
+    //   this.setState({
+    //     squares: squares,
+    //     xIsNext: ! this.state.xIsNext//X O가 번갈아서 나타게함.
+    //   });
+    // }
 
     renderSquare(i) {
       return (
         <Square
-          value={this.state.squares[i]}
-          onClick={()=>this.handleClick(i)}
+          value={this.props.squares[i]}
+          onClick={()=>this.props.onClick(i)}
         />
       );//Square에 value prop전달. squares의 값을 전달. 클릭하면 Board에서 Square로 함수를 전달.
     }
   
     render() {
       //승자가 결정되면 문구를 표시.
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner){
-        status = 'Winner: ' + winner;
-      } else{
-        status = 'Next player: ' + (this.state.xIsNext ? '❌' : '⭕');
-      }
+      // Game 컴포넌트가 게임의 상태를 렌더링.
+      // const winner = calculateWinner(this.state.squares);
+      // let status;
+      // if (winner){
+      //   status = 'Winner: ' + winner;
+      // } else{
+      //   status = 'Next player: ' + (this.state.xIsNext ? '❌' : '⭕');
+      // }
         
       return (
         <div>
-          <div className="status">{status}</div>
+          
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -99,15 +102,79 @@ var s="미사일라면방독면⏬"
   }
   
   class Game extends React.Component {//게임판을 렌더링.
+    //history로 이전 동작을 저장.
+    constructor(props){
+      super(props);
+      this.state={
+        history:[{
+          // squares:Array(9).fill(null),
+          squares:Array(...s),
+        }],
+        stepNumber:0,
+        xIsNext: true,
+      };
+    }
+    
+    handleClick(i){//square를 클릭하면 사각형을 채우게함.
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);//이후에 기록을 지움.
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();//불변성을 위해 slice로 사본을 만듬.
+
+      if (calculateWinner(squares) || (['❌' , '⭕']).includes( squares[i] ) ){//승자가 결정되거나, 채워진 칸일때.
+        return;
+      }
+      squares[i] = this.state.xIsNext ? '❌' : '⭕';
+      this.setState({
+        // squares: squares,
+        history: history.concat([{
+          squares:squares,
+        }]),//concat는 기존 배열을 복제 하지 않음.
+        stepNumber: history.length,
+        xIsNext: ! this.state.xIsNext//X O가 번갈아서 나타게함.
+      });
+    }
+
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      })
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move)=>{
+        const desc = move ?
+          'Go to move #' + move:
+          'Go to game start';
+        return(
+          <li key={move}>
+            <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
+      let status;
+      if (winner){
+        status = 'Winner: ' + winner;
+      } else{
+        status = 'Next player: ' + (this.state.xIsNext ? '❌' : '⭕');
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{ status }</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
