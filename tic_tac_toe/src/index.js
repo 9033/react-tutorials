86252f31345fd97518dbf6afdf72d3ba1c39499d
9033 <28668509+9033@users.noreply.github.com>
2019-12-15 13:02:24 +0900

    사용자와 상호작용하는 컴포넌트 만들기 index.js - Square 컴포넌트를 클릭하면 값을 표시.

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -10,13 +10,24 @@ prop라는 매개변수를 받고
 render함수를 통해 반환.
 */
 var s="미사일라면방독면⏬"
-class Square extends React.Component {//<button>을 렌더링.
+  class Square extends React.Component {//<button>을 렌더링. 클릭하면 경고창.
+    constructor(props){//무언가를 ㄱ하기 위해 컴포넌트는 state를 사용.
+      super (props);//생성자에서 super는 반드시.
+      this.state = {
+        value:null,
+      };
+    }
+
+    //setState가 호출되면 React에게 Square가 다시 렌더링하라고 알림.
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        <button
+          className="square" 
+          onClick={() => this.setState({value: this.props.value }) }
+        >
+          {this.state.value}
         </button>
-      );//prop을 전달 : 부모 -> 자식 컴포넌트.
+      );//prop을 전달 : 부모 -> 자식 컴포넌트. this.state.value 의 값을 렌더링.
     }
   }
   
